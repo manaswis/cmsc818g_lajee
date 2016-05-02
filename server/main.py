e7e9213c40a51b05ca30090a83fb0d10e5c178a9
Manaswi Saha <manaswi@cs.umd.edu>
2016-05-01 20:39:20 -0400

    Add base framework for the flask server app

---
+++ server/main.py
@@ -0,0 +1,32 @@
+"""`main` is the top level module for your Flask application."""
+
+# Import the Flask Framework
+from flask import Flask
+app = Flask(__name__)
+# Note: We don't need to call run() since our application is embedded within
+# the App Engine WSGI application server.
+
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'Hello CMSC818G people!'
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Return a custom 404 error."""
+    return 'Sorry, Nothing at this URL.', 404
+
+
+@app.errorhandler(500)
+def application_error(e):
+    """Return a custom 500 error."""
+    return 'Sorry, unexpected error: {}'.format(e), 500
+
+
+if __name__ == '__main__':
+    # This is used when running locally. Gunicorn is used to run the
+    # application on Google App Engine. See entrypoint in app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
+# [END app]
